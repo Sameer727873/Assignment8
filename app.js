6b9d788f2cc17c1df7dee73f8ed4f8bdb7ff475b
Sameer727873 <111059349+Sameer727873@users.noreply.github.com>
2022-09-02 03:54:27 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,212 @@
+
+              //Chapter 1
+// alert("Error! Please enter a valid Password");
+// alert("Welcome to Jsland.....\nHappy Codding.")
+// alert("Welcome to js land"); alert("Happy coding! \n Pervent this page from creating aditional dialogue.")
+// alert("Hello... I can run js through my web browser");
+
+//Chapter 2
+// var username = "Sam727873";
+// var myName = "sameerAhmed";
+// var messege = "Hello world";
+// alert(username)
+// alert(myName)
+// alert(messege)
+ 
+// var a = "Pizza\n"
+// var b = "Pizz\n"
+// var c ="Piz\n"
+// var d ="pi\n"
+// var e = "P\n"
+// var all= a+b+c+d+e
+// alert(all)
+
+// document.write("Yah!Ican write HTML Content through js")
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+//Chapter 3
+// var age = "my age is 15"; alert(age)
+// var a ="you have visited this site 14 times"; alert(a)
+// document.write("My birth year is 2002  Data type of me decleared variable is number")
+
+//chapter 4
+// var abc = "Legal"
+// var 123 = "Illigal"
+// var ab1= "legal"
+// var 1abc = "illigal"
+// var & = "illigal"
+// var -c = "illigal"
+// var _c = "ligal"
+// var .b = "illegal"
+// var $54 = "legal"
+
+//chapter 5
+// var a = 3 
+// var b = 5
+// document.write(a+b)
+
+// var a = 3 
+// var b = 5
+// document.write(a-b)
+
+// var a = 3 
+// var b = 5
+// document.write(a*b)
+// var a = 3 
+// var b = 5
+// document.write(a/b)
+
+// var a = 3 
+// var b = 5
+// document.write(a%b)
+
+// var a = 5
+// var b = 6
+// var c = ++a + ++b - a
+// alert(c)
+
+// var a = 5;
+// alert(a++ +a - --a)
+
+// var ticket1 = 600
+// var all = 5
+// var total =ticket1*all
+// alert("total cost to buy 5 ticket is"+total)
+
+// var English = +prompt("English","Enter your Marks");
+// var Urdu = +prompt("Urdu","Enter your Marks");
+// var Math = +prompt("Math","Enter your Marks");
+// var Science = +prompt("Science","Enter your Marks");
+// var Pst = +prompt("Pst","Enter your Marks");
+// var Obtained = English + Urdu +Math + Science + Pst ;
+// if (Obtained > 400 && Obtained < 500 ) {document.write("Your Rank is A1")}
+// else if (Obtained > 350 && Obtained < 400)  {document.write("Your Rank is A")}
+// else if (Obtained > 300 && Obtained < 350 ) {document.write ("Your RANK IS B")}
+// else if (Obtained > 250 && Obtained < 300) {document.write ("Your rank is C")}
+// else {document.write("You are Fail")}
+
+// var us = 104.80;
+// var pkr = +prompt("us to pkr");
+// document.write(us*pkr)
+// var saudi = 28;
+// var pkr = +prompt("saudi to pkr");
+// document.write(saudi*pkr)
+
+//chapter 6 to 8
+// var a = 10;
+// alert(++a + a++ + --a - --a)
+
+// var a = 2;
+// var b = 1;
+//  alert(--a - --b + ++b + b--) 
+
+//chapter 9 to 11
+// var city = prompt("Enter city name")
+// var city1 = "karachi"
+// var city2 = "peshawer"
+// if(city == "karachi"){document.write("Welcome to The city of light")}
+// else if(city == "peshawer"){document.write("Welcome to city of flower")}
+// else{document.write("welcome to city")}
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// var English = +prompt("English","Enter your Marks");
+// var Urdu = +prompt("Urdu","Enter your Marks");
+// var Math = +prompt("Math","Enter your Marks");
+// var Science = +prompt("Science","Enter your Marks");
+// var Pst = +prompt("Pst","Enter your Marks");
+// var Obtained = English + Urdu +Math + Science + Pst ;
+// if (Obtained > 400 && Obtained < 500 ) {document.write("Your Rank is A1")}
+// else if (Obtained > 350 && Obtained < 400)  {document.write("Your Rank is A")}
+// else if (Obtained > 300 && Obtained < 350 ) {document.write ("Your RANK IS B")}
+// else if (Obtained > 250 && Obtained < 300) {document.write ("Your rank is C")}
+// else {document.write("You are Fail")}
+//chapter 12to 13
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// document.write ("Good day");}
+// else{
+// document.write ("Good evening");
+// }
+
+     //chapter 14 to    16
+    // var students =["sana","saba","ali","umer","areeba","iqra","zainab",]
+    //  students.pop(4)
+    // students.push('wahid')
+    //students.shift()
+    //students.unshift('qasim')
+   // students.splice(0,2,'loki')
+//    students.slice(2,4)
+//     console.log(students)
+    
+    
+//     var students = []
+//     students[0] = "sana"
+//         students[1] = "saba"
+//         students[2] = "ali"
+// console.log(students)
+// var numbers = [12,34,56,78,90]
+// console.log(numbers)
+
+// var bulean = [true , false]
+// console.log(bulean)
+// var mix = ["sana","ali",12,true]
+// console.log(mix)
+//chapter 20
+
+// var a;
+// for(a=6; a<=20 ; a= a+3)
+// {document.write(a)}
+
+// for(x=10; x>=1 ; x--){
+//    console.log(x)
+// }
+// var a ;
+// for(a=1 ; a<=10 ; a++){
+//    document.write("4" + "x" + a + "=" + a*4 + "<br>")
+// }
+// var cities =["karachi","lahore","islamabad","peshawar"]
+// for(var i = 0 ; i <cities.length ; i++)
+//    {if(cities[i]==="karachi")
+
+// {
+//    alert(cities[i]+"is a city of light")
+
+// }
+//    }
+// for(var i = 1; i < 20 ; i++){
+//    for(var j = 1 ; j < 40 ; j++){
+//       document.write(j +"i am king" + "<br>")
+//    }
+// }
